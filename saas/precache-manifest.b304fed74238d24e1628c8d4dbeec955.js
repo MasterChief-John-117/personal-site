6a6a943b90dc91905860f90ccddf4a0232ef773f
Galen G <github@mastrchef.rocks>
2019-12-02 20:01:27 -0500

    add suffo

---
+++ saas/precache-manifest.b304fed74238d24e1628c8d4dbeec955.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85dc307ecb50223596f64e363e410248",
+    "url": "/saas/index.html"
+  },
+  {
+    "revision": "ae0825376eddc625d936",
+    "url": "/saas/static/css/main.d0f745f1.chunk.css"
+  },
+  {
+    "revision": "45e5187f5fe99ae98fb3",
+    "url": "/saas/static/js/2.77cd481c.chunk.js"
+  },
+  {
+    "revision": "ae0825376eddc625d936",
+    "url": "/saas/static/js/main.8e5f97a7.chunk.js"
+  },
+  {
+    "revision": "97f035f17e06e5333184",
+    "url": "/saas/static/js/runtime-main.bfbe64f4.js"
+  }
+]);
