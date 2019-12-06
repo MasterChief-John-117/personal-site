bd161bd04d8826cb58555abebd359170b3aa5876
Galen G <github@mastrchef.rocks>
2019-12-05 23:29:44 -0500

    Add spotify react app

---
+++ spotify/precache-manifest.595dcf0a3a494e2c2dc241c702dfa1b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b976413d778d9834fb824e0c64c998f",
+    "url": "/spotify/index.html"
+  },
+  {
+    "revision": "1e4b5b2d5c3ed4572a0a",
+    "url": "/spotify/static/css/main.e93684b8.chunk.css"
+  },
+  {
+    "revision": "e1e2b470bb7d1b880288",
+    "url": "/spotify/static/js/2.305d3a0f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spotify/static/js/2.305d3a0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e4b5b2d5c3ed4572a0a",
+    "url": "/spotify/static/js/main.148faf4f.chunk.js"
+  },
+  {
+    "revision": "5f791f56b33bdc9010d4",
+    "url": "/spotify/static/js/runtime-main.9da8cb0b.js"
+  }
+]);
