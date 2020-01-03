dac4d8866c6ffe316dae9ee80082af70b1c3fef0
Galen G <github@mastrchef.rocks>
2020-01-03 01:21:59 -0800

    Include build time

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 COMMIT=`git rev-parse HEAD | cut -c 1-8`
+TIME=`date +"%m-%d-%Y %T %Z"`
 
 rm -r build/
 mkdir build/
 cp * build/ -r
 cd build/
 
-# Insert Commit ID
+# Insert build info
 sed -i "s/[{][{]COMMIT[}][}]/$COMMIT/g" *.html
 sed -i "s/[{][{]COMMIT[}][}]/$COMMIT/g" */*.html
+sed -i "s/[{][{]TIME[}][}]/$TIME/g" *.html
+sed -i "s/[{][{]TIME[}][}]/$TIME/g" */*.html
 
 # Build Footer
 sed -i $'/[{][{]FOOTER[}][}]/{r footer.build.html\nd}' *.html
